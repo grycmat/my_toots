728b2c38eb64e71be431cd602354680ccec4e7d8
Mateusz Gryc <mateusz@gryc.dev>
2022-12-24 12:28:49 +0100

    implemented option to add multiple files to status

--- lib/widgets/compose_status.widget.dart
+++ lib/widgets/compose_status.widget.dart
@@ -19,7 +19,7 @@ class ComposeStatusWidget extends StatefulWidget {
 class _ComposeStatusWidgetState extends State<ComposeStatusWidget> {
   final _textEditingController = TextEditingController();
   int _chars = 0;
-  File? _media;
+  List<File>? _mediaFiles = [];
   final List<String> _mediaIds = [];
   @override
   void initState() {
@@ -59,85 +59,108 @@ class _ComposeStatusWidgetState extends State<ComposeStatusWidget> {
     return Container(
       height: MediaQuery.of(context).size.height * 0.95,
       color: Theme.of(context).colorScheme.surface,
-      child: Center(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(
-              8, 8, 8, MediaQuery.of(context).viewInsets.bottom),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                (widget.inReplyToStatus == null
-                    ? Container()
-                    : StatusWidget(status: widget.inReplyToStatus!)),
-                TextField(
-                  autofocus: true,
-                  controller: _textEditingController,
-                  maxLines: null,
-                  keyboardType: TextInputType.multiline,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      color: Theme.of(context).colorScheme.primary,
-                      onPressed: () {
-                        _postStatus(context);
-                      },
-                      icon: const Icon(Icons.send_outlined, size: 30),
-                    ),
-                    border: const OutlineInputBorder(),
-                    hintText: "Let's make some toots!",
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+            8, 8, 8, MediaQuery.of(context).viewInsets.bottom),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              (widget.inReplyToStatus == null
+                  ? Container()
+                  : StatusWidget(status: widget.inReplyToStatus!)),
+              TextField(
+                autofocus: true,
+                controller: _textEditingController,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    color: Theme.of(context).colorScheme.primary,
+                    onPressed: () {
+                      _postStatus(context);
+                    },
+                    icon: const Icon(Icons.send_outlined, size: 30),
                   ),
+                  border: const OutlineInputBorder(),
+                  hintText: "Let's toot something!",
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      _chars.toString(),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        FilePicker.platform
-                            .pickFiles()
-                            .then((FilePickerResult? result) {
-                          if (result != null) {
-                            File file = File(result.files.single.path!);
-                            print(file);
-                            getIt
-                                .get<ApiService>()
-                                .uploadFile(file)
-                                .then((response) {
-                              setState(() {
-                                _media = file;
-                                print(response.data);
-                                _mediaIds.add(response.data['id']);
-                              });
-                            });
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Uploading media aborted'),
-                              ),
-                            );
-                            // User canceled the picker
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    _chars.toString(),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      FilePicker.platform
+                          .pickFiles(allowMultiple: true)
+                          .then((FilePickerResult? result) {
+                        if (result != null) {
+                          final List<PlatformFile> selectedFiles = result.files;
+                          if (selectedFiles.isEmpty) {
+                            return;
                           }
-                        });
-                      },
-                      icon: Icon(Icons.add_photo_alternate_outlined,
-                          color: Theme.of(context).colorScheme.primary,
-                          size: 30),
+                          final List<File> files =
+                              selectedFiles.map((f) => File(f.path!)).toList();
+                          setState(() {
+                            _mediaFiles = files;
+                          });
+                          // getIt
+                          //     .get<ApiService>()
+                          //     .uploadFile(file)
+                          //     .then((response) {
+                          //   setState(() {
+                          //     _media = file;
+                          //     print(response.data);
+                          //     _mediaIds.add(response.data['id']);
+                          //   });
+                          // });
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Uploading media aborted'),
+                            ),
+                          );
+                          // User canceled the picker
+                        }
+                      });
+                    },
+                    icon: Icon(Icons.add_photo_alternate_outlined,
+                        color: Theme.of(context).colorScheme.primary, size: 30),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.poll_outlined,
+                      color: Theme.of(context).colorScheme.primary,
+                      size: 30,
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.poll_outlined,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: 30,
-                      ),
+                  ),
+                ],
+              ),
+              _mediaFiles!.isEmpty
+                  ? const SizedBox()
+                  : GridView.count(
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                      shrinkWrap: true,
+                      crossAxisCount: 2,
+                      children: _mediaFiles!
+                          .map(
+                            (File f) => SizedBox(
+                              width: 100,
+                              height: 100,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.file(f, fit: BoxFit.cover),
+                              ),
+                            ),
+                          )
+                          .toList(),
                     ),
-                  ],
-                ),
-                _media != null ? Image.file(_media!) : Container(),
-              ],
-            ),
+            ],
           ),
         ),
       ),
